fa44d5f4f6a558ea28cf3f3d3c7b801bfa831d4e
Robert Farfsing <robertfarfsing@gmail.com>
2020-02-12 16:34:57 -0500

    Add project files.

---
+++ BankingApplication/Program.cs
@@ -0,0 +1,13 @@
+﻿using BankingLibrary;
+using System;
+
+
+namespace BankingApplication {
+    class Program {
+        static void Main(string[] args) {
+            var chk = new Account();
+            chk.Deposit(100);
+            chk.Withdraw(75);
+        }
+    }
+}
